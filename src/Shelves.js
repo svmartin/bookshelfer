719b1bffcf41e980ba16ccc32f624e3325472b99
SM <sean.martin@gmail.com>
2017-10-02 15:45:17 -0500

    add Shelves

---
+++ src/Shelves.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+class Shelves extends React.Component {
+
+
+  render() {
+    return (
+      <div className="list-books">
+        <div className="list-books-title">
+          <h1>Mis Libros</h1>
+        </div>
+        <div className="list-books-content">
+          <div>
+
+          </div>
+        </div>
+        {/* add here ?*/}
+      </div>
+    );
+  }
+}
+export default Shelves;
