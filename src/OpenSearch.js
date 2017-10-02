53de374471a96708c5930c50ecaf3c9109d12240
SM <sean.martin@gmail.com>
2017-10-02 17:13:35 -0500

    add OpenSearch

---
+++ src/OpenSearch.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+const OpenSearch = () => {
+  return (
+    <div className="open-search">
+      <Link to="/search">Add a Book</Link
+    </div>
+  )
+}
+
+export default OpenSearch
