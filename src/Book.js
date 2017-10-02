11d2e532d204b8d7a65384756c19aa29c3b428dd
SM <sean.martin@gmail.com>
2017-10-02 17:56:33 -0500

    create Book

---
+++ src/Book.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class Book extends React.Component {
+
+  render() {
+    return (
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover">
+            <div/>
+          </div>
+          <div className="book-title">Test Book Title</div>
+        </div>
+      </li>
+    )
+  }
+}
+
+export default Book
