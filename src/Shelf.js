d0ed9f702992afdd210638dfb6e0f7003822133b
SM <sean.martin@gmail.com>
2017-10-02 17:19:07 -0500

    add Shelf

---
+++ src/Shelf.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+class Shelf extends React.Component {
+  render() {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title"></h2>
+        <ol className="bookshelf-books">
+          <li>test book</li>
+        </ol>
+      </div>
+    )
+  }
+}
