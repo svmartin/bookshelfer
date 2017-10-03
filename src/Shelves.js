baea19fb9f5b461665a842d898ac26cf349dc1f6
SM <sean.martin@gmail.com>
2017-10-03 15:37:03 -0500

    rename sections

--- src/Shelves.js
+++ src/Shelves.js
@@ -14,9 +14,9 @@ class Shelves extends React.Component {
         </div>
         <div className="list-books-content">
           <div>
-            <ListBooksOnShelf section={"Want to Read"} books={this.props.books}/>
-            <ListBooksOnShelf section={"Currently Reading"} books={this.props.books}/>
-            <ListBooksOnShelf section={"Have Read"}
+            <ListBooksOnShelf section={"wantToRead"} books={this.props.books}/>
+            <ListBooksOnShelf section={"currentlyReading"} books={this.props.books}/>
+            <ListBooksOnShelf section={"read"}
             books={this.props.books}/>
           </div>
         </div>
